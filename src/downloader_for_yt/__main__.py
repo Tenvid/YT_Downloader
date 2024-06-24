f72aed78214d0aaa83e872e4c171b1e183668b97
David <89795659+Tenvid@users.noreply.github.com>
2024-06-24 15:55:58 +0200

    Refactor and add help to arguments

--- src/downloader_for_yt/__main__.py
+++ src/downloader_for_yt/__main__.py
@@ -1,3 +1,6 @@
+import re
+import sys
+from argparse import Namespace
 from pathlib import Path
 
 import cmd2
@@ -5,36 +8,55 @@ import yt_dlp
 
 BASE_PATH = Path.cwd() / "videos"
 
-url = "https://youtu.be/UYXULc8E5fs?si=SbLVl-91F3eULrmu"
-
-
-def main():
-    print(BASE_PATH)
-
-    yt = yt_dlp.YoutubeDL(
-        {"paths": {"home": str(BASE_PATH)}, "listformats": True}
-    )
-    yt.download(url)
-
-
-# if __name__ == "__main__":
-#     main()
-
 
 class DownloaderApp(cmd2.Cmd):
     """CMD2 application for the video downloading"""
 
-    def do_list_all_formats(self, line: cmd2.parsing.Statement):
+    def validate_url(self, url: str) -> bool:
+        """Check if the url is valid.
+
+        Args:
+            url (str): Url to be checked
+
+        Returns:
+            bool: If url is valid
+        """
+
+        return re.match("(https:\/\/youtu\.be\/)([a-zA-Z0-9?=\-_]*)", url)
+
+    def do_list_all_formats(self, line: Namespace):
+        """Shows all the available formats for the given url.
+
+        Args:
+            line (Namespace): Line arguments
+        """
+
         yt = yt_dlp.YoutubeDL({"listformats": True})
 
         yt.download(str(line))
 
     download_parser = cmd2.Cmd2ArgumentParser()
-    download_parser.add_argument("url", type=str)
-    download_parser.add_argument("-p", "--path", type=str)
+    download_parser.add_argument(
+        "url", type=str, help="Url which will be downloaded."
+    )
+    download_parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        help="Path of the folder where the video will be downloaded. Can be absolute or relative.",
+    )
 
     @cmd2.with_argparser(download_parser)
-    def do_default_download(self, line: cmd2.parsing.Statement):
+    def do_default_download(self, line: Namespace):
+        """Download given video url. A path can be specified.
+
+        Args:
+            line (Namespace): Line arguments
+        """
+
+        if not self.validate_url(line.url):
+            return
+
         path = line.path if line.path else str(BASE_PATH)
 
         yt = yt_dlp.YoutubeDL({"paths": {"home": path}})
@@ -42,17 +64,30 @@ class DownloaderApp(cmd2.Cmd):
         yt.download(str(line.url))
 
     download_parser_with_format = cmd2.Cmd2ArgumentParser()
-    download_parser_with_format.add_argument("url", type=str)
-    download_parser_with_format.add_argument("-p", "--path", type=str)
+    download_parser_with_format.add_argument(
+        "url", type=str, help="Url which will be downloaded."
+    )
+    download_parser_with_format.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        help="Path of the folder where the video will be downloaded. Can be absolute or relative.",
+    )
     download_parser_with_format.add_argument(
         "-f",
         "--format",
         type=str,
-        help="Format for the download. It should be like <video-format-code>+<audio-format-code>",
+        help="Format for the download. It should be like <video-format-code>+<audio-format-code>.",
     )
 
     @cmd2.with_argparser(download_parser_with_format)
-    def do_download_with_format(self, line: cmd2.parsing.Statement):
+    def do_download_with_format(self, line: Namespace):
+        """Download video with specified format. A path can be specified.
+
+        Args:
+            line (Namespace): Line arguments
+        """
+
         path = line.path if line.path else str(BASE_PATH)
 
         yt = yt_dlp.YoutubeDL(
@@ -63,7 +98,5 @@ class DownloaderApp(cmd2.Cmd):
 
 
 if __name__ == "__main__":
-    import sys
-
-    c = DownloaderApp()
-    sys.exit(c.cmdloop())
+    app = DownloaderApp()
+    sys.exit(app.cmdloop())
